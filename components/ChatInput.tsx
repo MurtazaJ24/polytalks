25955be7128a1d1c16e770c41e5f930a41461b45
Murtaza <murtazamj2003@gmail.com>
2023-12-05 00:00:12 +0530

    added padding to chat input

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -48,7 +48,7 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder={`Message ${chatPartner.username}`}
-          className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 sm:text-sm sm:leading-6"
+          className="block w-full resize-none border-0 bg-transparent text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:py-1.5 px-2 sm:text-sm sm:leading-6"
         />
 
         <div
